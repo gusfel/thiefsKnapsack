1d557abacdec788eb7269efc615f199a16962de4
gusfel <agustinjfeliciano@gmail.com>
2021-04-14 10:52:54 -0700

    testing

--- solution.js
+++ solution.js
@@ -1,3 +1,18 @@
+/*Thief's Knapsack
+
+A Thief has a knapsack that can hold X lbs of stolen goods
+Each stolen good is worth a certain amount of cash, but
+the stolen good also weighs a certain weight. This means that
+the thief has to pick an optimal combination of items!
+The Thief can't pick the same item twice.
+
+What is the maximum worth of goods that the thief can steal?
+*/
 const thiefPack = (packCap, itemValArr, itemWeightArr) => {
-  
-}
+  let testVal = 0;
+  let maxVal = 0;
+
+  const helper = arr => {
+    for
+  };
+};
