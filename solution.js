235420a2f997aa52a736782195aacca880f6b355
gusfel <agustinjfeliciano@gmail.com>
2021-04-14 12:20:55 -0700

    trying with my original solution

--- solution.js
+++ solution.js
@@ -9,47 +9,48 @@ The Thief can't pick the same item twice.
 What is the maximum worth of goods that the thief can steal?
 */
 
-
 const thiefPack = (packCap, itemValArr, itemWeightArr) => {
-  const currIndex = itemValArr.length - 1;
-  if (packCap <= 0 || currIndex - 1 === -1) {
-    return 0;
-  }
-  if (itemWeightArr[currIndex] > packCap) {
-    return thiefPack(itemValArr, itemWeightArr, packCap);
-  } else {
-    let included = values[currIndex] + thiefPack(packCap - itemWeightArr[currIndex], itemValArr, itemWeightArr);
-    let excluded = thiefPack(packCap, itemValArr, itemWeightArr);
-    return Math.max(included, excluded);
-  }
+
+  let maxVal = 0;
+
+  const helper = (arrW, arrV, testVal, testWeight) => {
+
+    for (let i = 0; i < arrW.length; i++) {
+      // let testVal = 0;
+      // let testWeight = 0;
+      const currWeight = arrW[i];
+      const currVal = arrV[i];
+      testVal += currVal;
+      testWeight += currWeight;
+      if (testWeight > packCap) {
+        let testVal = 0;
+        let testWeight = 0;
+        return;
+      }
+      if (testWeight < packCap && testVal > maxVal) {
+        maxVal = testVal;
+        helper(arrW.splice(i, 1), arrV.splice(i, 1), testVal, testWeight);
+      }
+    }
+  };
+
+  helper(itemValArr, itemWeightArr, 0, 0);
+  return maxVal;
 };
 
 
+
 // const thiefPack = (packCap, itemValArr, itemWeightArr) => {
-
-//   let maxVal = 0;
-
-//   const helper = (arrW, arrV, testVal, testWeight) => {
-
-//     for (let i = 0; i < arrW.length; i++) {
-//       // let testVal = 0;
-//       // let testWeight = 0;
-//       const currWeight = arrW[i];
-//       const currVal = arrV[i];
-//       testVal += currVal;
-//       testWeight += currWeight;
-//       if (testWeight > packCap) {
-//         let testVal = 0;
-//         let testWeight = 0;
-//         return;
-//       }
-//       if (testWeight < packCap && testVal > maxVal) {
-//         maxVal = testVal;
-//         helper(arrW.splice(i, 1), arrV.splice(i, 1), testVal, testWeight);
-//       }
-//     }
-//   };
-
-//   helper(itemValArr, itemWeightArr, 0, 0);
-//   return maxVal;
+//   const currIndex = itemValArr.length - 1;
+//   if (packCap <= 0 || itemValArr.length === 0) {
+//     return 0;
+//   }
+//   if (itemWeightArr[currIndex] > packCap) {
+//     return thiefPack(packCap, itemValArr, itemWeightArr);
+//   } else {
+//     let withItem = values[currIndex] + thiefPack(packCap - itemWeightArr[currIndex], itemValArr, itemWeightArr);
+//     let noItem = thiefPack(packCap, itemValArr, itemWeightArr);
+//     return Math.max(withItem, noItem);
+//   }
 // };
+
